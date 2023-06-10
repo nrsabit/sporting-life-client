30b8e9e8205a8c7b53b96ec30251f623aa8456b6
nrsabit <mailbox.sabit@gmail.com>
2023-06-10 07:00:31 +0600

    initial routes setup completed

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,9 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
+import Main from "../components/Layouts/Main/Main";
+import Home from "../components/Pages/Home/Home/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello Guys</div>,
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
   },
 ]);
 
