2e15dacb6d075ad90cc1aadc848c1a2386a58bcb
nrsabit <mailbox.sabit@gmail.com>
2023-06-13 20:56:40 +0600

    manage users section completed

---
+++ src/components/Pages/ManageUsers/ManageUserCard.jsx
@@ -0,0 +1,72 @@
+import React, { useRef } from "react";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import Swal from "sweetalert2";
+
+const ManageUserCard = ({ user, refetch }) => {
+  const { image, name, role, email, _id } = user;
+  const axiosSecure = useAxiosSecure();
+
+  const handleUpdateUser = (role) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, Update it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure
+          .patch(`/update-user/${_id}?role=${role}`)
+          .then((data) => {
+            if (data.data.modifiedCount > 0) {
+              Swal.fire(
+                "Updated!",
+                "User Role Updated Successfully",
+                "success"
+              );
+              refetch();
+            }
+          });
+      }
+    });
+  };
+
+  return (
+    <div
+      className={`rounded p-4 shadow-lg mb-6 md:flex gap-4 text-[#213644] bg-base-100`}
+    >
+      <div className="flex flex-col justify-center items-center md:w-1/3">
+        <img className="w-40 h-40 rounded-full" src={image} alt="" />
+      </div>
+      <div className="flex flex-col justify-center items-center my-4 md:my-0 md:w-1/3">
+        <h3 className="text-2xl font-semibold text-center">{name}</h3>
+        <p className="font-bold text-center my-2">
+          Email: <span className="font-normal">{email}</span>
+        </p>
+        <p className="font-bold text-center">
+          Role: <span className="font-normal">{role}</span>
+        </p>
+      </div>
+      <div className="flex flex-col justify-center items-center md:w-1/3">
+        <button
+          onClick={() => handleUpdateUser("admin")}
+          disabled={role === "admin"}
+          className="btn btn-sm bg-green-500 text-white"
+        >
+          Make Admin
+        </button>
+        <button
+          onClick={() => handleUpdateUser("instructor")}
+          disabled={role === "instructor"}
+          className="btn btn-sm mt-2 bg-blue-700 text-white"
+        >
+          Make Instructor
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ManageUserCard;
--- src/components/Pages/ManageUsers/ManageUsers.jsx
+++ src/components/Pages/ManageUsers/ManageUsers.jsx
@@ -1,43 +1,61 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { AuthContext } from "../../../providers/AuthProvider";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
+import { Helmet } from "react-helmet-async";
+import ManageUserCard from "./ManageUserCard";
 
 const ManageUsers = () => {
-    const { user, loading } = useContext(AuthContext);
-    const axiosSecure = useAxiosSecure();
-  
-    const {
-      data: allClasses = [],
-      refetch,
-      isLoading,
-    } = useQuery(["all-classes"], async () => {
-      if (!loading && user?.email) {
-        const res = await axiosSecure.get(`/all-classes`);
-        return res.data;
-      }
-    });
-  
-    if (isLoading) {
-      return (
-        <div className="min-h-screen">
-          <img
-            className="mx-auto"
-            src="https://i.ibb.co/GFy0712/loading.gif"
-            alt=""
-          />
-        </div>
-      );
-    }
-  
-    if (allClasses.length === 0 && !isLoading) {
-      return (
-        <h2 className="text-4xl font-bold text-[#213644] text-center my-10">
-          No Enrolled Classes Found
-        </h2>
-      );
-    }
+  const { user, loading } = useContext(AuthContext);
+  const axiosSecure = useAxiosSecure();
 
+  const {
+    data: allUsers = [],
+    refetch,
+    isLoading,
+  } = useQuery(["users"], async () => {
+    if (!loading && user?.email) {
+      const res = await axiosSecure.get(`/users`);
+      return res.data;
+    }
+  });
+
+  if (isLoading) {
     return (
-        <div></div>
-    )
+      <div className="min-h-screen">
+        <img
+          className="mx-auto"
+          src="https://i.ibb.co/GFy0712/loading.gif"
+          alt=""
+        />
+      </div>
+    );
+  }
+
+  if (allUsers.length === 0 && !isLoading) {
+    return (
+      <h2 className="text-4xl font-bold text-[#213644] text-center my-10">
+        No Users Found
+      </h2>
+    );
+  }
+  return (
+    <div className="w-full p-8 md:w-3/5 mx-auto">
+      <Helmet>
+        <title>Sporting Life | Manage Users</title>
+      </Helmet>
+      <h2 className="text-4xl font-bold text-[#213644] text-center my-10">
+        Manage Users
+      </h2>
+      {allUsers.map((user) => (
+        <ManageUserCard
+          key={user._id}
+          user={user}
+          refetch={refetch}
+        ></ManageUserCard>
+      ))}
+    </div>
+  );
 };
 
 export default ManageUsers;
